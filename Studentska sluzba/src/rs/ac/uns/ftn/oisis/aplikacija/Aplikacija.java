df19b92e9a717699d3326b709b9a24e7d6cf9acc
Djordjije Kundacina <qndaa@uns.ac.rs>
2019-12-03 01:42:15 +0100

    #glavni prozor

--- Studentska sluzba/src/rs/ac/uns/ftn/oisis/aplikacija/Aplikacija.java
+++ Studentska sluzba/src/rs/ac/uns/ftn/oisis/aplikacija/Aplikacija.java
@@ -1,10 +1,13 @@
 package rs.ac.uns.ftn.oisis.aplikacija;
 
+import rs.ac.uns.ftn.oisis.gui.MainFrame;
+
 public class Aplikacija {
 
 	public static void main(String[] args) {
 		// Start :D 
-
+		MainFrame mb = new MainFrame();
+		mb.setVisible(true);
 	}
 
 }
---
+++ Studentska sluzba/src/rs/ac/uns/ftn/oisis/gui/MainFrame.java
@@ -0,0 +1,36 @@
+package rs.ac.uns.ftn.oisis.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	private static final long serialVersionUID = -6400016063430161422L;
+
+	public MainFrame () {
+		super();
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenWidth = screenSize.width;
+		int screenHeight = screenSize.height;
+		
+		setSize(screenWidth*3/4, screenHeight*3/4);
+		setTitle("Studentska sluzba");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Color bg = new Color(210, 210, 210);
+		getContentPane().setBackground(bg);
+		
+		setResizable(false);
+		
+		
+		
+		
+		
+		
+	}
+}
