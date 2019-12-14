91bcf6f7c4df1830c3e833709dd28a7b0c6ad78a
Djordjije Kundacina <qndaa@uns.ac.rs>
2019-12-14 21:25:00 +0100

    #profesor

---
+++ Studentska sluzba/src/rs/ac/uns/ftn/oisis/model/Osoba.java
@@ -0,0 +1,70 @@
+package rs.ac.uns.ftn.oisis.model;
+
+public abstract class Osoba {
+	private String ime;
+	private String prezime;
+	private String datumRodjenja;
+	private String adresaStanovanja;
+	private String kontaktTelefon;
+	private String email;
+	
+	public Osoba(String ime, String prezime, String datumRodjenja, String adresaStanovanja, String kontaktTelefon,
+			String email) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+		this.datumRodjenja = datumRodjenja;
+		this.adresaStanovanja = adresaStanovanja;
+		this.kontaktTelefon = kontaktTelefon;
+		this.email = email;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(String datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+
+	public String getKontaktTelefon() {
+		return kontaktTelefon;
+	}
+
+	public void setKontaktTelefon(String kontaktTelefon) {
+		this.kontaktTelefon = kontaktTelefon;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+}
--- Studentska sluzba/src/rs/ac/uns/ftn/oisis/model/Profesor.java
+++ Studentska sluzba/src/rs/ac/uns/ftn/oisis/model/Profesor.java
@@ -1,5 +1,63 @@
 package rs.ac.uns.ftn.oisis.model;
 
-public class Profesor {
+import java.util.ArrayList;
 
+public class Profesor extends Osoba {
+
+	private String adresaKancelarije;
+	private String brojLicneKarte;
+	private String titula;
+	private String zvanje;
+	private ArrayList<Predmet> predajeNaPredmetima;
+	
+	public Profesor(String ime, String prezime, String datumRodjenja, String adresaStanovanja, String kontaktTelefon,
+			String email, String adresaKancelarije, String brojLicneKarte, String titula, String zvanje) {
+		super(ime, prezime, datumRodjenja, adresaStanovanja, kontaktTelefon, email);
+		this.adresaKancelarije = adresaKancelarije;
+		this.brojLicneKarte = brojLicneKarte;
+		this.titula = titula;
+		this.zvanje = zvanje;
+		this.predajeNaPredmetima = new ArrayList<Predmet>();
+	}
+
+	public String getAdresaKancelarije() {
+		return adresaKancelarije;
+	}
+
+	public void setAdresaKancelarije(String adresaKancelarije) {
+		this.adresaKancelarije = adresaKancelarije;
+	}
+
+	public String getBrojLicneKarte() {
+		return brojLicneKarte;
+	}
+
+	public void setBrojLicneKarte(String brojLicneKarte) {
+		this.brojLicneKarte = brojLicneKarte;
+	}
+
+	public String getTitula() {
+		return titula;
+	}
+
+	public void setTitula(String titula) {
+		this.titula = titula;
+	}
+
+	public String getZvanje() {
+		return zvanje;
+	}
+
+	public void setZvanje(String zvanje) {
+		this.zvanje = zvanje;
+	}
+
+	public ArrayList<Predmet> getPredajeNaPredmetima() {
+		return predajeNaPredmetima;
+	}
+
+	public void setPredajeNaPredmetima(ArrayList<Predmet> predajeNaPredmetima) {
+		this.predajeNaPredmetima = predajeNaPredmetima;
+	}
+		
 }
